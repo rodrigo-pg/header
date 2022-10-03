7a4a042c0bd85dbfe6120b48b0f1da0ce0e8d4e3
uKn70 <rodrigopgrodrigues@outlook.com>
2022-10-02 23:09:58 -0300

    updated

--- src/gateway/api/catalogs/index.ts
+++
@@ -1,9 +0,0 @@
-import { makeGetAllCatalogsController } from "../../../catalogs/main/factories/get-all-catalogs";
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(request: VercelRequest, response: VercelResponse) {
-    const controller = await makeGetAllCatalogsController();
-    const data = await controller.handle(request);
-
-    response.status(data.statusCode).json({...data.body});
-}
